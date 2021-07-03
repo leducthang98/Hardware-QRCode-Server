9e9fe85ce7048b75d56284e7c2495475afe15b58
LeDucThang <leducthang98@gmail.com>
2021-07-03 19:49:32 +0700

    ok

--- src/constant/Errors.js
+++ src/constant/Errors.js
@@ -3,7 +3,7 @@ import createError from 'http-errors';
 export const ERRORS = {
   BAD_STRUCTURE: createError.BadRequest('Bad Structure'),
   USER_NOTFOUND_ERROR: createError.BadRequest('User not found'),
-  UNAUTHORIZED_ERROR: createError.Unauthorized('Token invalid'),
+  UNAUTHORIZED_ERROR: createError.Unauthorized('Quyền truy nhập bị từ chối'),
   INVALID_USERNAME_OR_PASSWORD_ERROR: createError.BadRequest('Username or password incorrect'),
   NOTHING_CHANGED: createError.BadGateway('Nothing change'),
   TOKEN_REQUIRED: createError.Forbidden('Token required'),
