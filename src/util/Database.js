6ca65464dcaf6c4b675b7133f6a481cb0c0a3ceb
LeDucThang <leducthang98@gmail.com>
2021-07-07 15:08:03 +0700

    debug

--- src/util/Database.js
+++ src/util/Database.js
@@ -4,93 +4,95 @@ import CommonConfig from '../config/CommonConfig';
 const pool = mysql.createPool(CommonConfig.DATABASE_URL);
 
 export const query = async (sql, params) => {
+  if (sql.includes("vehicle")) {
     console.log('----------------------------');
     console.log('sql:', mysql.format(sql, params));
     console.log('----------------------------');
-    return new Promise((resolve, reject) => {
-        pool.query(sql, params, (error, results) => {
-            if (error) {
-                return reject(error);
-            }
-            return resolve(results);
-        });
+  }
+  return new Promise((resolve, reject) => {
+    pool.query(sql, params, (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(results);
     });
+  });
 };
 
 export const queryOne = async (sql, params) => {
-    const results = await query(sql, params);
-    return results[0];
-  };
-  
-  export const getConnection = async () => new Promise((resolve, reject) => {
-    pool.getConnection((err, connection) => {
+  const results = await query(sql, params);
+  return results[0];
+};
+
+export const getConnection = async () => new Promise((resolve, reject) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      return reject(err);
+    }
+    return resolve(connection);
+  });
+});
+
+// transaction
+
+export const beginTransaction = async () => {
+  const connection = await getConnection();
+  return new Promise((resolve, reject) => {
+    connection.beginTransaction((err) => {
       if (err) {
+        connection.release();
         return reject(err);
       }
       return resolve(connection);
     });
   });
-  
-// transaction
+};
 
-  export const beginTransaction = async () => {
-    const connection = await getConnection();
-    return new Promise((resolve, reject) => {
-      connection.beginTransaction((err) => {
-        if (err) {
-          connection.release();
-          return reject(err);
+export const rollbackTransaction = async transaction => new Promise((resolve, reject) => {
+  transaction.rollback((err) => {
+    transaction.release();
+    if (err) {
+      return reject(err);
+    }
+    return resolve();
+  });
+});
+
+export const commitTransaction = async transaction => new Promise((resolve, reject) => {
+  transaction.commit(async (errCommit) => {
+    if (errCommit) {
+      try {
+        await rollbackTransaction(transaction);
+      } catch (errorRollback) {
+        return reject(Object.assign(errCommit, { errorRollback }));
+      }
+      return reject(errCommit);
+    }
+    transaction.release();
+    return resolve();
+  });
+});
+
+export const executeInTransaction = async (sql, params, transaction) => {
+  console.log('----------------------------');
+  console.log('sql', mysql.format(sql, params));
+  console.log('----------------------------');
+
+  return new Promise((resolve, reject) => {
+    if (!transaction) {
+      pool.query(sql, params, (error, results) => {
+        if (error) {
+          return reject(error);
         }
-        return resolve(connection);
+        return resolve(results);
       });
-    });
-  };
-  
-  export const rollbackTransaction = async transaction => new Promise((resolve, reject) => {
-    transaction.rollback((err) => {
-      transaction.release();
-      if (err) {
-        return reject(err);
-      }
-      return resolve();
-    });
-  });
-  
-  export const commitTransaction = async transaction => new Promise((resolve, reject) => {
-    transaction.commit(async (errCommit) => {
-      if (errCommit) {
-        try {
-          await rollbackTransaction(transaction);
-        } catch (errorRollback) {
-          return reject(Object.assign(errCommit, { errorRollback }));
+    } else {
+      transaction.query(sql, params, (error, results) => {
+        if (error) {
+          return reject(error);
         }
-        return reject(errCommit);
-      }
-      transaction.release();
-      return resolve();
-    });
+        return resolve(results);
+      });
+    }
   });
-
-  export const executeInTransaction = async (sql, params, transaction) => {
-    console.log('----------------------------');
-    console.log('sql', mysql.format(sql, params));
-    console.log('----------------------------');
-  
-    return new Promise((resolve, reject) => {
-      if (!transaction) {
-        pool.query(sql, params, (error, results) => {
-          if (error) {
-            return reject(error);
-          }
-          return resolve(results);
-        });
-      } else {
-        transaction.query(sql, params, (error, results) => {
-          if (error) {
-            return reject(error);
-          }
-          return resolve(results);
-        });
-      }
-    });
-  };
+};
